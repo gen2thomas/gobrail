d78940af74f09619cdaeb5f61151eb435d54a468
Thomas Kohler <g2t@e.mail.de>
2021-04-16 20:03:53 +0200

    add tests

--- internal/boardsapi/boardsapi_test.go
+++ internal/boardsapi/boardsapi_test.go
@@ -36,30 +36,82 @@ var boardRecipeUnknown = BoardRecipe{
 	BoardType:   TypUnknown,
 }
 
-func TestNewBoardsAPI(t *testing.T) {
+func TestBoardsAPINew(t *testing.T) {
+	// arrange
+	assert := assert.New(t)
+	// act
+	api := NewBoardsAPI(new(adaptorMock))
+	// assert
+	assert.Equal(0, len(api.boards))
+	assert.Equal(0, len(api.usedPins))
+}
+
+func TestBoardsAPIAddBoard(t *testing.T) {
 	// arrange
 	assert := assert.New(t)
 	require := require.New(t)
-	// act
 	api := NewBoardsAPI(new(adaptorMock))
-	api.AddBoard(boardRecipeTyp2)
+	// act
+	err := api.AddBoard(boardRecipeTyp2)
 	// assert
+	require.Nil(err)
 	require.Equal(1, len(api.boards))
 	require.Equal(1, len(api.usedPins))
 	assert.Equal(0, len(api.usedPins[boardRecipeTyp2.Name]))
 	assert.Equal("Boards: 1\nBoard Id: TestRecipeTyp2, Name: TestRecipeTyp2, Chips: 1, Pins: 16\n\n", fmt.Sprintf("%s", api))
 }
 
-func TestNewBoardsAPIWithUnknownTypeGetsEmptyBoards(t *testing.T) {
+func TestBoardsAPIAddBoardReAddFails(t *testing.T) {
+	// arrange
+	assert := assert.New(t)
+	require := require.New(t)
+	api := &BoardsAPI{
+		usedPins: make(map[string]boardpin.PinNumbers),
+		boards:   make(BoardsMap),
+	}
+	api.boards["TestRecipeTyp2"] = &boardsMock{}
+	// act
+	err := api.AddBoard(boardRecipeTyp2)
+	// assert
+	require.NotNil(err)
+	assert.Contains(err.Error(), "Board already there")
+}
+
+func TestBoardsAPIAddBoardWithUnknownTypeGetsEmptyBoards(t *testing.T) {
 	// arrange
 	assert := assert.New(t)
 	require := require.New(t)
 	// act
 	api := NewBoardsAPI(new(adaptorMock))
-	api.AddBoard(boardRecipeUnknown)
+	err := api.AddBoard(boardRecipeUnknown)
 	// assert
+	require.NotNil(err)
 	require.NotNil(*api)
+	assert.Contains(err.Error(), "Unknown type")
 	assert.Equal("No Boards\n", fmt.Sprintf("%s", api))
+}
+
+func TestBoardsAPIRemoveBoard(t *testing.T) {
+	// arrange
+	assert := assert.New(t)
+	require := require.New(t)
+	api := &BoardsAPI{
+		usedPins: make(map[string]boardpin.PinNumbers),
+		boards:   make(BoardsMap),
+	}
+	api.boards["TestB1"] = &boardsMock{}
+	api.usedPins["TestB1"] = make(boardpin.PinNumbers)
+	api.usedPins["TestB1"][uint8(1)] = struct{}{}
+	api.boards["TestB2"] = &boardsMock{}
+	api.usedPins["TestB2"] = make(boardpin.PinNumbers)
+	api.usedPins["TestB2"][uint8(2)] = struct{}{}
+	// act
+	api.RemoveBoard("TestB1")
+	// assert
+	require.Equal(1, len(api.boards))
+	require.Equal(1, len(api.usedPins))
+	assert.NotNil(api.boards["TestB2"])
+	assert.NotNil(api.usedPins["TestB2"])
 }
 
 func TestGetFreePinsWithoutBoardGetsEmptyList(t *testing.T) {
@@ -78,12 +130,11 @@ func TestGetFreePinsWithoutBoardGetsEmptyList(t *testing.T) {
 func TestGetFreePins(t *testing.T) {
 	// arrange
 	assert := assert.New(t)
-	bm := make(BoardsMap)
-	bm["TestBoard1"] = &boardsMock{name: "TestBoard1", binPins: 2, anaPins: 5, memPins: 1}
 	api := &BoardsAPI{
 		usedPins: make(map[string]boardpin.PinNumbers),
-		boards:   bm,
+		boards:   make(BoardsMap),
 	}
+	api.boards["TestBoard1"] = &boardsMock{name: "TestBoard1", binPins: 2, anaPins: 5, memPins: 1}
 	// act
 	fp := api.GetFreePins("TestBoard1")
 	// assert
@@ -155,6 +206,43 @@ func TestGetInputPinWhenAlreadyUsedGetsError(t *testing.T) {
 	api.usedPins["TestBoard"][1] = struct{}{}
 	// act
 	pin, err := api.GetInputPin("TestBoard", 1)
+	// assert
+	require.NotNil(err)
+	assert.Nil(pin)
+	assert.Contains(err.Error(), "already used")
+}
+
+func TestGetOutputPin(t *testing.T) {
+	// arrange
+	assert := assert.New(t)
+	require := require.New(t)
+	api := &BoardsAPI{
+		boards:   make(BoardsMap),
+		usedPins: make(map[string]boardpin.PinNumbers),
+	}
+	api.boards["TestBoard"] = &boardsMock{name: "TestBoard", binPins: 1, anaPins: 1, memPins: 1}
+	api.usedPins["TestBoard"] = make(boardpin.PinNumbers)
+	// act
+	pin, err := api.GetOutputPin("TestBoard", 1)
+	// assert
+	require.Nil(err)
+	assert.NotNil(pin)
+	assert.Equal(1, len(api.usedPins))
+}
+
+func TestGetOutputPinWhenAlreadyUsedGetsError(t *testing.T) {
+	// arrange
+	assert := assert.New(t)
+	require := require.New(t)
+	api := &BoardsAPI{
+		boards:   make(BoardsMap),
+		usedPins: make(map[string]boardpin.PinNumbers),
+	}
+	api.boards["TestBoard"] = &boardsMock{name: "TestBoard"}
+	api.usedPins["TestBoard"] = make(boardpin.PinNumbers)
+	api.usedPins["TestBoard"][2] = struct{}{}
+	// act
+	pin, err := api.GetOutputPin("TestBoard", 2)
 	// assert
 	require.NotNil(err)
 	assert.Nil(pin)
