c35fad4fa62901acbdfeba7531c0e983ebf3828b
Thomas Kohler <g2t@e.mail.de>
2021-04-17 14:48:35 +0200

    add tests

--- internal/board/bord_typ2_test.go
+++ internal/board/bord_typ2_test.go
@@ -6,6 +6,8 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"gobot.io/x/gobot/drivers/i2c"
+
+	"github.com/gen2thomas/gobrail/internal/boardpin"
 )
 
 type adaptorMock struct {
@@ -21,6 +23,46 @@ func TestNewBoardTyp2(t *testing.T) {
 	// assert
 	require.NotNil(boardt2)
 	assert.Equal("TestNewBoardTyp2", boardt2.name)
+}
+
+func TestWriteGPIOWithoutDriverFails(t *testing.T) {
+	// arrange
+	assert := assert.New(t)
+	boardt2 := &Board{}
+	// act
+	err := boardt2.writeGPIO(&boardpin.Pin{}, 2)
+	// assert
+	assert.NotNil(err)
+}
+
+func TestReadGPIOWithoutDriverFails(t *testing.T) {
+	// arrange
+	assert := assert.New(t)
+	boardt2 := &Board{}
+	// act
+	_, err := boardt2.readGPIO(&boardpin.Pin{})
+	// assert
+	assert.NotNil(err)
+}
+
+func TestWriteEEPROMWithoutDriverFails(t *testing.T) {
+	// arrange
+	assert := assert.New(t)
+	boardt2 := &Board{}
+	// act
+	err := boardt2.writeEEPROM(&boardpin.Pin{}, 1)
+	// assert
+	assert.NotNil(err)
+}
+
+func TestReadEEPROMWithoutDriverFails(t *testing.T) {
+	// arrange
+	assert := assert.New(t)
+	boardt2 := &Board{}
+	// act
+	_, err := boardt2.readEEPROM(&boardpin.Pin{})
+	// assert
+	assert.NotNil(err)
 }
 
 func (a *adaptorMock) GetConnection(address int, bus int) (device i2c.Connection, err error) { return }
